755ae5445aeb7e76f9e6e1ac4671c20a3eb6942c
kellyson71 <kellyson.medeiros.pdf@gmail.com>
2025-06-13 16:11:30 -0300

    Adicionar verificação de usuário na autenticação

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -13,13 +13,19 @@ class UsuarioController extends Controller
 	public function autenticar(Request $request)
 	{
 		$suap = new SUAPClient();
+
 		try {
+			$usuario = Professor::where('matricula', $request->matricula)->first();
+			if ($usuario == null) {
+				session()->flash('flash', ['tipo' => 'danger', 'mensagem' => 'Usuário não autorizado a acessar o sistema.']);
+				return redirect('/');
+			}
+
 			$token = $suap->autenticar($request->matricula, $request->senha);
 			$dados = $suap->getMeusDados();
 			if ($dados['vinculo']['campus'] == Config::get('app.campus')) {
-				$usuario = Professor::where('matricula', $request->matricula)->first();
-				if ($usuario == null) {
-					$usuario = new Professor(['nome' => $dados['nome_usual'], 'matricula' => $dados['matricula']]);
+				if ($usuario->nome !== $dados['nome_usual']) {
+					$usuario->nome = $dados['nome_usual'];
 					$usuario->save();
 				}
 				session()->put('usuario', $usuario);
@@ -31,16 +37,23 @@ class UsuarioController extends Controller
 		}
 		return redirect('/');
 	}
-
-	public function sair() {
+	public function sair()
+	{
 		session()->forget('usuario');
 		return redirect('/');
 	}
+
 	public function entrar(Request $request)
 	{
 		$matricula = $request->input('matricula');
 		$senha = $request->input('senha');
+
 		try {
+			$usuario = Professor::where('matricula', $matricula)->first();
+			if ($usuario == null) {
+				return response()->json(['erro' => 'Usuário não autorizado a acessar o sistema.'], 403);
+			}
+
 			$suap = new SUAPClient();
 			$resultado = $suap->autenticar($matricula, $senha);
 
@@ -48,9 +61,8 @@ class UsuarioController extends Controller
 				$dados = $suap->getMeusDados();
 				$campusConfig = Config::get('app.campus');
 				if ($dados['vinculo']['campus'] == $campusConfig) {
-					$usuario = Professor::where('matricula', $request->matricula)->first();
-					if ($usuario == null) {
-						$usuario = new Professor(['nome' => $dados['nome_usual'], 'matricula' => $dados['matricula']]);
+					if ($usuario->nome !== $dados['nome_usual']) {
+						$usuario->nome = $dados['nome_usual'];
 						$usuario->save();
 					}
 					session()->put('usuario', $usuario);
