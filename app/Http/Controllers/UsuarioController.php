f2a0465cfa4542442d5b803babec354f7cfc3cce
kellyson71 <kellyson.medeiros.pdf@gmail.com>
2025-06-13 10:02:33 -0300

    Permitir acesso ao sistema para qualquer categoria de usuário (docente, aluno, etc.)

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -16,14 +16,7 @@ class UsuarioController extends Controller
 		try {
 			$token = $suap->autenticar($request->matricula, $request->senha);
 			$dados = $suap->getMeusDados();
-			if (($dados['vinculo']['categoria'] ?? '') == 'docente' && $dados['vinculo']['campus'] == Config::get('app.campus')) {
-				$usuario = Professor::where('matricula', $request->matricula)->first();
-				if ($usuario == null) {
-					$usuario = new Professor(['nome' => $dados['nome_usual'], 'matricula' => $dados['matricula']]);
-					$usuario->save();
-				}
-				session()->put('usuario', $usuario);
-			} elseif ($dados['tipo_vinculo'] == 'Aluno' && $dados['vinculo']['campus'] == Config::get('app.campus')) {
+			if ($dados['vinculo']['campus'] == Config::get('app.campus')) {
 				$usuario = Professor::where('matricula', $request->matricula)->first();
 				if ($usuario == null) {
 					$usuario = new Professor(['nome' => $dados['nome_usual'], 'matricula' => $dados['matricula']]);
@@ -31,7 +24,7 @@ class UsuarioController extends Controller
 				}
 				session()->put('usuario', $usuario);
 			} else {
-				session()->flash('flash', ['tipo' => 'danger', 'mensagem' => 'Você não é docente e/ou não pertence ao campus '.Config::get('app.campus').'.']);
+				session()->flash('flash', ['tipo' => 'danger', 'mensagem' => 'Você não pertence ao campus ' . Config::get('app.campus') . '.']);
 			}
 		} catch (\Exception $e) {
 			session()->flash('flash', ['tipo' => 'danger', 'mensagem' => 'Matrícula e/ou senha inválido(s).']);
@@ -53,9 +46,8 @@ class UsuarioController extends Controller
 
 			if ($resultado) {
 				$dados = $suap->getMeusDados();
-
 				$campusConfig = Config::get('app.campus');
-				if (($dados['vinculo']['categoria'] == 'docente' || $dados['tipo_vinculo'] == 'Aluno') && $dados['vinculo']['campus'] == $campusConfig) {
+				if ($dados['vinculo']['campus'] == $campusConfig) {
 					$usuario = Professor::where('matricula', $request->matricula)->first();
 					if ($usuario == null) {
 						$usuario = new Professor(['nome' => $dados['nome_usual'], 'matricula' => $dados['matricula']]);
@@ -63,7 +55,7 @@ class UsuarioController extends Controller
 					}
 					session()->put('usuario', $usuario);
 				} else {
-					return response()->json(['erro' => 'Matrícula e/ou senha inválido(s).'], 422);
+					return response()->json(['erro' => 'Você não pertence ao campus ' . $campusConfig . '.'], 422);
 				}
 			} else {
 				return response()->json(['erro' => 'Matrícula e/ou senha inválido(s).'], 422);
